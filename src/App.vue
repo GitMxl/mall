0c09948dd63af68af3aa4781e769a565a6ba8a20
mxl <811866517@qq.com>
2021-04-05 12:04:36 +0800

    'skjkd'

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
   </div>
 </template>
 <script>
-import MainTabBar from 'components/tabbar/MainTabBar'
+import MainTabBar from 'components/content/MainTabBar'
 export default {  
   name:'app',
   components:{
